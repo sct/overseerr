2519ac86f7bb6925e5646bec39cd66bd6a1bdd0b
sct <sctsnipe@gmail.com>
2020-08-16 04:40:38 +0000

    Add tailwindcss

---
+++ stylelint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "tailwind",
+          "apply",
+          "variants",
+          "responsive",
+          "screen",
+        ],
+      },
+    ],
+    "declaration-block-trailing-semicolon": null,
+    "no-descending-specificity": null,
+  },
+};
