ec33c639cddac9aea2a4ae56051dc19f0b6ee02a
sct <sctsnipe@gmail.com>
2020-08-17 02:01:15 +0000

    chore: fixed some prettier formatting errors in postcss.config.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { NextPage } from 'next';
 
 const Index: NextPage = () => {
-  return <div>Overseerr!</div>;
+  return (
+    <div className="bg-blue-700 mx-4 my-2 px-4 py-2 w-64">
+      <h1 className="text-xl">Overseer</h1>
+      <p className="py-4">Here is some text</p>
+    </div>
+  );
 };
 
 export default Index;
