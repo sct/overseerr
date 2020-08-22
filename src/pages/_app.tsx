0151134949d112f5f8a18c98c3ff6e3418221041
Alexander Zoitos <azoitos1@gmail.com>
2020-08-22 19:55:00 -0400

    refactor(use component on login path conditional): use component on login path conditional

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import '../styles/globals.css';
 import App from 'next/app';
 import Layout from '../components/Layout';
-import LoginPage from './login';
 
 class CoreApp extends App {
   public render(): JSX.Element {
     const { Component, pageProps, router } = this.props;
-    if (router.asPath == '/login') {
-      return <LoginPage {...pageProps} />;
+    if (router.asPath === '/login') {
+      return <Component {...pageProps} />;
     } else {
       return (
         <Layout>
