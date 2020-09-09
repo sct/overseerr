8280846c6a78910406257113d717d94fd860753b
sct <sctsnipe@gmail.com>
2020-09-09 00:23:58 +0000

    style(frontend): login page styling touchup

---
+++ src/types/custom.d.ts
@@ -0,0 +1,24 @@
+/* eslint-disable */
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: any;
+  export default content;
+}
+declare module '*.jpeg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
