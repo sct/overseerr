978f92a1c589ac404a3cb1103a68a8a5ffb0dd7d
sct <sctsnipe@gmail.com>
2020-11-11 09:06:42 +0000

    feat: season creation migration

---
+++ server/migration/1605085519544-SeasonStatus.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class SeasonStatus1605085519544 implements MigrationInterface {
+  name = 'SeasonStatus1605085519544';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "season" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "seasonNumber" integer NOT NULL, "status" integer NOT NULL DEFAULT (1), "createdAt" datetime NOT NULL DEFAULT (datetime('now')), "updatedAt" datetime NOT NULL DEFAULT (datetime('now')), "mediaId" integer)`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "season"`);
+  }
+}
