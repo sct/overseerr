baf9b11f14f3bceb0dc230663c30114ecf440000
Brandon Cohen <cohbrandon@gmail.com>
2020-11-18 23:13:22 -0500

    fix(frontend): fixed similar/recommendations showing when empty

--- src/components/MovieDetails/index.tsx
+++ src/components/MovieDetails/index.tsx
@@ -491,98 +491,106 @@ const MovieDetails: React.FC<MovieDetailsProps> = ({ movie }) => {
           />
         ))}
       />
-      <div className="md:flex md:items-center md:justify-between mb-4 mt-6">
-        <div className="flex-1 min-w-0">
-          <Link
-            href="/movie/[movieId]/recommendations"
-            as={`/movie/${data.id}/recommendations`}
-          >
-            <a className="inline-flex text-xl leading-7 text-cool-gray-300 hover:text-white sm:text-2xl sm:leading-9 sm:truncate items-center">
-              <span>
-                <FormattedMessage {...messages.recommendations} />
-              </span>
-              <svg
-                className="w-6 h-6 ml-2"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+      {(recommended?.results ?? []).length > 0 && (
+        <>
+          <div className="md:flex md:items-center md:justify-between mb-4 mt-6">
+            <div className="flex-1 min-w-0">
+              <Link
+                href="/movie/[movieId]/recommendations"
+                as={`/movie/${data.id}/recommendations`}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </a>
-          </Link>
-        </div>
-      </div>
-      <Slider
-        sliderKey="recommendations"
-        isLoading={!recommended && !recommendedError}
-        isEmpty={false}
-        items={recommended?.results.map((title) => (
-          <TitleCard
-            key={`recommended-${title.id}`}
-            id={title.id}
-            image={title.posterPath}
-            status={title.mediaInfo?.status}
-            summary={title.overview}
-            title={title.title}
-            userScore={title.voteAverage}
-            year={title.releaseDate}
-            mediaType={title.mediaType}
+                <a className="inline-flex text-xl leading-7 text-cool-gray-300 hover:text-white sm:text-2xl sm:leading-9 sm:truncate items-center">
+                  <span>
+                    <FormattedMessage {...messages.recommendations} />
+                  </span>
+                  <svg
+                    className="w-6 h-6 ml-2"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </a>
+              </Link>
+            </div>
+          </div>
+          <Slider
+            sliderKey="recommendations"
+            isLoading={!recommended && !recommendedError}
+            isEmpty={false}
+            items={recommended?.results.map((title) => (
+              <TitleCard
+                key={`recommended-${title.id}`}
+                id={title.id}
+                image={title.posterPath}
+                status={title.mediaInfo?.status}
+                summary={title.overview}
+                title={title.title}
+                userScore={title.voteAverage}
+                year={title.releaseDate}
+                mediaType={title.mediaType}
+              />
+            ))}
           />
-        ))}
-      />
-      <div className="md:flex md:items-center md:justify-between mb-4 mt-6">
-        <div className="flex-1 min-w-0">
-          <Link
-            href="/movie/[movieId]/similar"
-            as={`/movie/${data.id}/similar`}
-          >
-            <a className="inline-flex text-xl leading-7 text-cool-gray-300 hover:text-white sm:text-2xl sm:leading-9 sm:truncate items-center">
-              <span>
-                <FormattedMessage {...messages.similar} />
-              </span>
-              <svg
-                className="w-6 h-6 ml-2"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+        </>
+      )}
+      {(similar?.results ?? []).length > 0 && (
+        <>
+          <div className="md:flex md:items-center md:justify-between mb-4 mt-6">
+            <div className="flex-1 min-w-0">
+              <Link
+                href="/movie/[movieId]/similar"
+                as={`/movie/${data.id}/similar`}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </a>
-          </Link>
-        </div>
-      </div>
-      <Slider
-        sliderKey="similar"
-        isLoading={!similar && !similarError}
-        isEmpty={false}
-        items={similar?.results.map((title) => (
-          <TitleCard
-            key={`recommended-${title.id}`}
-            id={title.id}
-            image={title.posterPath}
-            status={title.mediaInfo?.status}
-            summary={title.overview}
-            title={title.title}
-            userScore={title.voteAverage}
-            year={title.releaseDate}
-            mediaType={title.mediaType}
+                <a className="inline-flex text-xl leading-7 text-cool-gray-300 hover:text-white sm:text-2xl sm:leading-9 sm:truncate items-center">
+                  <span>
+                    <FormattedMessage {...messages.similar} />
+                  </span>
+                  <svg
+                    className="w-6 h-6 ml-2"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                </a>
+              </Link>
+            </div>
+          </div>
+          <Slider
+            sliderKey="similar"
+            isLoading={!similar && !similarError}
+            isEmpty={false}
+            items={similar?.results.map((title) => (
+              <TitleCard
+                key={`recommended-${title.id}`}
+                id={title.id}
+                image={title.posterPath}
+                status={title.mediaInfo?.status}
+                summary={title.overview}
+                title={title.title}
+                userScore={title.voteAverage}
+                year={title.releaseDate}
+                mediaType={title.mediaType}
+              />
+            ))}
           />
-        ))}
-      />
+        </>
+      )}
       <div className="pb-8" />
     </div>
   );
