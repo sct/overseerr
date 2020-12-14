edbbccf3ae623430294f1a5c3fd2728dbd42e555
sct <sctsnipe@gmail.com>
2020-12-14 08:01:33 +0000

    fix(plex sync): catch errors that occur during processMovie
    this also removes the unique constraint on imdbId
    
    re #244 #246 #250

---
+++ server/migration/1607928251245-DropImdbIdConstraint.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner, TableUnique } from 'typeorm';
+
+export class DropImdbIdConstraint1607928251245 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropUniqueConstraint(
+      'media',
+      'UQ_7ff2d11f6a83cb52386eaebe74b'
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createUniqueConstraint(
+      'media',
+      new TableUnique({
+        name: 'UQ_7ff2d11f6a83cb52386eaebe74b',
+        columnNames: ['imdbId'],
+      })
+    );
+  }
+}
