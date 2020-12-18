5e5ba4050563f07bff367d2fb31ed7e7fca4291e
sct <sctsnipe@gmail.com>
2020-12-19 00:12:41 +0900

    fix: change default internal port to 5055 (#389)

--- src/pages/movie/[movieId]/index.tsx
+++ src/pages/movie/[movieId]/index.tsx
@@ -17,7 +17,7 @@ MoviePage.getInitialProps = async (ctx) => {
   if (ctx.req) {
     const cookies = parseCookies(ctx);
     const response = await axios.get<MovieDetailsType>(
-      `http://localhost:${process.env.PORT || 3000}/api/v1/movie/${
+      `http://localhost:${process.env.PORT || 5055}/api/v1/movie/${
         ctx.query.movieId
       }${cookies.locale ? `?language=${cookies.locale}` : ''}`,
       {
