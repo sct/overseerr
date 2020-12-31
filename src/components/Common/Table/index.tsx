ef7b8906d7d1da11cfb18a296a44904e7fe2bad6
Brandon Cohen <cohbrandon@gmail.com>
2020-12-30 23:42:27 -0500

    fix(frontend): revered change to keep tables edge to edge on mobile

--- src/components/Common/Table/index.tsx
+++ src/components/Common/Table/index.tsx
@@ -71,7 +71,7 @@ const TD: React.FC<TDProps> = ({
 const Table: React.FC = ({ children }) => {
   return (
     <div className="flex flex-col">
-      <div className="my-2 overflow-x-auto">
+      <div className="my-2 overflow-x-auto -mx-6 md:mx-0 lg:mx-0">
         <div className="py-2 align-middle inline-block min-w-full">
           <div className="shadow overflow-hidden sm:rounded-lg">
             <table className="min-w-full">{children}</table>
