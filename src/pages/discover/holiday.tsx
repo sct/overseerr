22f2037ea6c5a0ba2ffa4d69f2b7cf42bdcf8575
sct <sctsnipe@gmail.com>
2021-01-02 08:33:24 +0000

    fix(holiday): remove special holiday slider

--- src/pages/discover/holiday.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { NextPage } from 'next';
-import Holiday from '../../components/Discover/Holiday';
-
-const HolidayPage: NextPage = () => {
-  return <Holiday />;
-};
-
-export default HolidayPage;
