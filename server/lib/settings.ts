e431fb1a31ff76475c6d420ab9d500e5768aee3b
Chris Pritchard <chris@christopherpritchard.co.uk>
2021-01-12 17:29:08 +0000

    feat(settings/plex): added ability to retrieve plex servers from plex.tv
    Retrieve server settings from plex.tv, including checking server connectivity. Includes a refactor (non-breaking) of the plex connection code to enable settings to be passed to it without saving them (e.g. to test manual configuration without saving).
    
    fix #289

--- server/lib/settings.ts
+++ server/lib/settings.ts
@@ -19,6 +19,12 @@ export interface PlexSettings {
   libraries: Library[];
 }
 
+export interface PlexStatus {
+  settings: PlexSettings;
+  status: number;
+  message: string;
+}
+
 interface DVRSettings {
   id: number;
   name: string;
@@ -115,6 +121,44 @@ export interface NotificationAgentWebhook extends NotificationAgentConfig {
     jsonPayload: string;
     authHeader: string;
   };
+}
+
+export interface PlexConnection {
+  protocol: string;
+  address: string;
+  port: number;
+  uri: string;
+  local: boolean;
+  status?: number;
+  message?: string;
+  host?: string;
+}
+
+export interface PlexDevice {
+  name: string;
+  product: string;
+  productVersion: string;
+  platform: string;
+  platformVersion: string;
+  device: string;
+  clientIdentifier: string;
+  createdAt: Date;
+  lastSeenAt: Date;
+  provides: string[];
+  owned: boolean;
+  accessToken?: string;
+  publicAddress?: string;
+  httpsRequired?: boolean;
+  synced?: boolean;
+  relay?: boolean;
+  dnsRebindingProtection?: boolean;
+  natLoopbackSupported?: boolean;
+  publicAddressMatches?: boolean;
+  presence?: boolean;
+  ownerID?: string;
+  home?: boolean;
+  sourceTitle?: string;
+  connection: PlexConnection[];
 }
 
 interface NotificationAgents {
