c36cb8999249a1d178d06285b18af40e09b2a3b3
Danshil Mungur <danshil.mungur@gmail.com>
2021-02-08 19:50:03 +0400

    added check against empty string for tv show ratings

--- src/components/TvDetails/index.tsx
+++ src/components/TvDetails/index.tsx
@@ -135,7 +135,12 @@ const TvDetails: React.FC<TvDetailsProps> = ({ tv }) => {
 
   const seriesAttributes: React.ReactNode[] = [];
 
-  if (data.contentRatings.results.length) {
+  if (
+    data.contentRatings.results.length &&
+    data.contentRatings.results.find(
+      (r) => r.iso_3166_1 === 'US' || data.contentRatings.results[0].rating
+    )
+  ) {
     seriesAttributes.push(
       <span className="p-0.5 py-0 border rounded-md">
         {data.contentRatings.results.find((r) => r.iso_3166_1 === 'US')
