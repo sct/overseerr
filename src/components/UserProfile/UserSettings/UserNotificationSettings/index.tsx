e71031205c3a560a8038269431a78d939d58c0ce
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-02-18 19:29:04 +0000

    fix(ui): Add tip & validation for Discord ID input

--- src/components/UserProfile/UserSettings/UserNotificationSettings/index.tsx
+++ src/components/UserProfile/UserSettings/UserNotificationSettings/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Field, Form, Formik } from 'formik';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 import { useToasts } from 'react-toast-notifications';
 import useSWR from 'swr';
@@ -10,11 +10,16 @@ import Error from '../../../../pages/_error';
 import Button from '../../../Common/Button';
 import LoadingSpinner from '../../../Common/LoadingSpinner';
 import { UserSettingsNotificationsResponse } from '../../../../../server/interfaces/api/userSettingsInterfaces';
+import * as Yup from 'yup';
+import { LanguageContext } from '../../../../context/LanguageContext';
 
 const messages = defineMessages({
   notificationsettings: 'Notification Settings',
   enableNotifications: 'Enable Notifications',
-  discordId: 'Discord ID',
+  discordId: 'Discord User ID',
+  discordIdTip:
+    'The <FindDiscordIdLink>18-digit ID number</FindDiscordIdLink> for your Discord user account',
+  validationDiscordId: 'You must provide a valid Discord user ID',
   save: 'Save Changes',
   saving: 'Saving…',
   plexuser: 'Plex User',
@@ -25,12 +30,20 @@ const messages = defineMessages({
 
 const UserNotificationSettings: React.FC = () => {
   const intl = useIntl();
+  const { locale } = useContext(LanguageContext);
   const { addToast } = useToasts();
   const router = useRouter();
   const { user, mutate } = useUser({ id: Number(router.query.userId) });
   const { data, error, revalidate } = useSWR<UserSettingsNotificationsResponse>(
     user ? `/api/v1/user/${user?.id}/settings/notifications` : null
   );
+
+  const UserNotificationSettingsSchema = Yup.object().shape({
+    discordId: Yup.string().matches(
+      /\d{18}/,
+      intl.formatMessage(messages.validationDiscordId)
+    ),
+  });
 
   if (!data && !error) {
     return <LoadingSpinner />;
@@ -52,6 +65,7 @@ const UserNotificationSettings: React.FC = () => {
           enableNotifications: data?.enableNotifications,
           discordId: data?.discordId,
         }}
+        validationSchema={UserNotificationSettingsSchema}
         enableReinitialize
         onSubmit={async (values) => {
           try {
@@ -97,7 +111,46 @@ const UserNotificationSettings: React.FC = () => {
               </div>
               <div className="form-row">
                 <label htmlFor="discordId" className="text-label">
-                  {intl.formatMessage(messages.discordId)}
+                  <span>{intl.formatMessage(messages.discordId)}</span>
+                  <span className="label-tip">
+                    {intl.formatMessage(messages.discordIdTip, {
+                      FindDiscordIdLink: function FindDiscordIdLink(msg) {
+                        const discordSupportLocales = [
+                          'da',
+                          'de',
+                          'es',
+                          'fr',
+                          'it',
+                          'ja',
+                          'ko',
+                          'nl',
+                          'pl',
+                          'pt-br',
+                          'ru',
+                          'sv',
+                          'tr',
+                          'zh-tw',
+                        ];
+
+                        return (
+                          <a
+                            href={`https://support.discord.com/hc/${
+                              discordSupportLocales.includes(
+                                locale.toLowerCase()
+                              )
+                                ? locale.toLowerCase()
+                                : 'en-us'
+                            }/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-`}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="text-gray-100 underline transition duration-300 hover:text-white"
+                          >
+                            {msg}
+                          </a>
+                        );
+                      },
+                    })}
+                  </span>
                 </label>
                 <div className="form-input">
                   <div className="flex max-w-lg rounded-md shadow-sm">
