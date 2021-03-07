be9d2806bbd716af2d287f10b413d215674e1aba
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-03-06 19:51:34 -0500

    fix: add default variants to Tailwind scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,7 +63,7 @@ module.exports = {
     margin: ['first', 'last', 'responsive'],
     boxShadow: ['group-focus'],
     opacity: ['disabled', 'hover', 'group-hover'],
-    scale: ['group-hover'],
+    scale: ['hover', 'focus', 'group-hover'],
     zIndex: ['hover', 'responsive'],
   },
   plugins: [
