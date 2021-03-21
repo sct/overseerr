0d4460a30a41d0f28109193cd6de955f1ddece29
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-03-21 17:51:33 -0400

    feat(ui): add default quota setting options

--- src/components/Settings/SettingsUsers/index.tsx
+++ src/components/Settings/SettingsUsers/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import useSWR from 'swr';
-import LoadingSpinner from '../../Common/LoadingSpinner';
-import type { MainSettings } from '../../../../server/lib/settings';
-import { Form, Formik, Field } from 'formik';
 import axios from 'axios';
-import Button from '../../Common/Button';
+import { Field, Form, Formik } from 'formik';
+import React from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 import { useToasts } from 'react-toast-notifications';
-import PermissionEdit from '../../PermissionEdit';
-import PageTitle from '../../Common/PageTitle';
+import useSWR from 'swr';
+import type { MainSettings } from '../../../../server/lib/settings';
 import globalMessages from '../../../i18n/globalMessages';
+import Button from '../../Common/Button';
+import LoadingSpinner from '../../Common/LoadingSpinner';
+import PageTitle from '../../Common/PageTitle';
+import PermissionEdit from '../../PermissionEdit';
 
 const messages = defineMessages({
   users: 'Users',
@@ -19,8 +19,15 @@ const messages = defineMessages({
   saving: 'Saving…',
   toastSettingsSuccess: 'User settings saved successfully!',
   toastSettingsFailure: 'Something went wrong while saving settings.',
-  localLogin: 'Enable Local User Sign-In',
-  defaultPermissions: 'Default User Permissions',
+  localLogin: 'Enable Local Sign-In',
+  movieRequestLimitLabel: 'Default Movie Request Limit',
+  movieRequestLimit: '{quotaLimit} movies per {quotaDays} days',
+  tvRequestLimitLabel: 'Default Series Request Limit',
+  tvRequestLimit: '{quotaLimit} seasons per {quotaDays} days',
+  defaultPermissions: 'Default Permissions',
+  unlimited: 'Unlimited',
+  validationQuotaLimit: 'You must enter a valid request limit',
+  validationQuotaDays: 'You must enter a valid number of days',
 });
 
 const SettingsUsers: React.FC = () => {
@@ -52,6 +59,10 @@ const SettingsUsers: React.FC = () => {
         <Formik
           initialValues={{
             localLogin: data?.localLogin,
+            movieQuotaLimit: data?.defaultQuotas.movie.quotaLimit ?? 0,
+            movieQuotaDays: data?.defaultQuotas.movie.quotaDays ?? 7,
+            tvQuotaLimit: data?.defaultQuotas.tv.quotaLimit ?? 0,
+            tvQuotaDays: data?.defaultQuotas.tv.quotaDays ?? 7,
             defaultPermissions: data?.defaultPermissions ?? 0,
           }}
           enableReinitialize
@@ -59,6 +70,16 @@ const SettingsUsers: React.FC = () => {
             try {
               await axios.post('/api/v1/settings/main', {
                 localLogin: values.localLogin,
+                defaultQuotas: {
+                  movie: {
+                    quotaLimit: values.movieQuotaLimit,
+                    quotaDays: values.movieQuotaDays,
+                  },
+                  tv: {
+                    quotaLimit: values.tvQuotaLimit,
+                    quotaDays: values.tvQuotaDays,
+                  },
+                },
                 defaultPermissions: values.defaultPermissions,
               });
 
@@ -92,6 +113,92 @@ const SettingsUsers: React.FC = () => {
                         setFieldValue('localLogin', !values.localLogin);
                       }}
                     />
+                  </div>
+                </div>
+                <div className="form-row">
+                  <label htmlFor="applicationTitle" className="text-label">
+                    {intl.formatMessage(messages.movieRequestLimitLabel)}
+                  </label>
+                  <div className="form-input">
+                    {intl.formatMessage(messages.movieRequestLimit, {
+                      quotaLimit: (
+                        <Field
+                          as="select"
+                          id="movieQuotaLimit"
+                          name="movieQuotaLimit"
+                          className="inline short"
+                        >
+                          <option value="0">
+                            {intl.formatMessage(messages.unlimited)}
+                          </option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="5">5</option>
+                          <option value="10">10</option>
+                          <option value="25">25</option>
+                          <option value="50">50</option>
+                          <option value="100">100</option>
+                        </Field>
+                      ),
+                      quotaDays: (
+                        <Field
+                          as="select"
+                          id="movieQuotaDays"
+                          name="movieQuotaDays"
+                          className="inline short"
+                        >
+                          <option value="1">1</option>
+                          <option value="7">7</option>
+                          <option value="14">14</option>
+                          <option value="30">30</option>
+                          <option value="60">60</option>
+                          <option value="90">90</option>
+                        </Field>
+                      ),
+                    })}
+                  </div>
+                </div>
+                <div className="form-row">
+                  <label htmlFor="applicationTitle" className="text-label">
+                    {intl.formatMessage(messages.tvRequestLimitLabel)}
+                  </label>
+                  <div className="form-input">
+                    {intl.formatMessage(messages.tvRequestLimit, {
+                      quotaLimit: (
+                        <Field
+                          as="select"
+                          id="tvQuotaLimit"
+                          name="tvQuotaLimit"
+                          className="inline short"
+                        >
+                          <option value="0">
+                            {intl.formatMessage(messages.unlimited)}
+                          </option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="5">5</option>
+                          <option value="10">10</option>
+                          <option value="25">25</option>
+                          <option value="50">50</option>
+                          <option value="100">100</option>
+                        </Field>
+                      ),
+                      quotaDays: (
+                        <Field
+                          as="select"
+                          id="tvQuotaDays"
+                          name="tvQuotaDays"
+                          className="inline short"
+                        >
+                          <option value="1">1</option>
+                          <option value="7">7</option>
+                          <option value="14">14</option>
+                          <option value="30">30</option>
+                          <option value="60">60</option>
+                          <option value="90">90</option>
+                        </Field>
+                      ),
+                    })}
                   </div>
                 </div>
                 <div
