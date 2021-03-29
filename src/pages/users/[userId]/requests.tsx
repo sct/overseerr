7153f4e9e892369d8f92fdd1da27df8147c09115
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-03-28 23:38:43 -0400

    fix(frontend): also allow REQUEST_VIEW perm through route guard

--- src/pages/users/[userId]/requests.tsx
+++ src/pages/users/[userId]/requests.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import { NextPage } from 'next';
+import React from 'react';
 import RequestList from '../../../components/RequestList';
 import useRouteGuard from '../../../hooks/useRouteGuard';
 import { Permission } from '../../../hooks/useUser';
 
 const UserRequestsPage: NextPage = () => {
-  useRouteGuard(Permission.MANAGE_REQUESTS);
+  useRouteGuard([Permission.MANAGE_REQUESTS, Permission.REQUEST_VIEW], {
+    type: 'or',
+  });
   return <RequestList />;
 };
 
