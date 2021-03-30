5c135c9974ebfcbdb434dafd459d1035624df6ed
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-03-30 03:51:33 -0400

    fix(ui): use correct colspan for 'No results.' message in Settings > Logs (#1325)
    * fix(ui): use correct colspan for 'No results.' message in Settings > Logs
    
    * fix(ui): also fix colspan of pagination row

--- src/components/Settings/SettingsLogs/index.tsx
+++ src/components/Settings/SettingsLogs/index.tsx
@@ -397,7 +397,7 @@ const SettingsLogs: React.FC = () => {
 
             {data.results.length === 0 && (
               <tr className="relative h-24 p-2 text-white">
-                <Table.TD colSpan={4} noPadding>
+                <Table.TD colSpan={5} noPadding>
                   <div className="flex flex-col items-center justify-center w-screen p-6 lg:w-full">
                     <span className="text-base">
                       {intl.formatMessage(globalMessages.noresults)}
@@ -418,7 +418,7 @@ const SettingsLogs: React.FC = () => {
               </tr>
             )}
             <tr className="bg-gray-700">
-              <Table.TD colSpan={6} noPadding>
+              <Table.TD colSpan={5} noPadding>
                 <nav
                   className="flex flex-col items-center w-screen px-6 py-3 space-x-4 space-y-3 sm:space-y-0 sm:flex-row lg:w-full"
                   aria-label="Pagination"
