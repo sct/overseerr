85c6d2ac28dac4064a99e640a10cb7054f6be923
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-04-12 10:40:29 -0400

    fix: remove route guard from profile pages

--- src/pages/profile/settings/notifications/discord.tsx
+++ src/pages/profile/settings/notifications/discord.tsx
@@ -3,11 +3,8 @@ import React from 'react';
 import UserSettings from '../../../../components/UserProfile/UserSettings';
 import UserNotificationSettings from '../../../../components/UserProfile/UserSettings/UserNotificationSettings';
 import UserNotificationsDiscord from '../../../../components/UserProfile/UserSettings/UserNotificationSettings/UserNotificationsDiscord';
-import useRouteGuard from '../../../../hooks/useRouteGuard';
-import { Permission } from '../../../../hooks/useUser';
 
 const NotificationsPage: NextPage = () => {
-  useRouteGuard(Permission.MANAGE_USERS);
   return (
     <UserSettings>
       <UserNotificationSettings>
