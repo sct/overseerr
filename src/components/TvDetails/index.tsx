4b196f19f7101ad1331a1f1d4cf4aff63cd9e1e9
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-04-13 01:00:18 -0400

    fix(lang): remove unused strings & correct manageModalNoRequests strings

--- src/components/TvDetails/index.tsx
+++ src/components/TvDetails/index.tsx
@@ -49,7 +49,7 @@ const messages = defineMessages({
   overviewunavailable: 'Overview unavailable.',
   manageModalTitle: 'Manage Series',
   manageModalRequests: 'Requests',
-  manageModalNoRequests: 'No Requests',
+  manageModalNoRequests: 'No requests.',
   manageModalClearMedia: 'Clear All Media Data',
   manageModalClearMediaWarning:
     '* This will irreversibly remove all data for this TV series, including any requests. If this item exists in your Plex library, the media information will be recreated during the next scan.',
