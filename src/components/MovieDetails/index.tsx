3a7ccc37f8ad7f01253dbaef4b8e5924a24a21db
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-04-23 18:24:54 -0400

    feat: display release dates for theatrical, digital, and physical release types

--- src/components/MovieDetails/index.tsx
+++ src/components/MovieDetails/index.tsx
@@ -1,8 +1,10 @@
 import {
   ArrowCircleRightIcon,
+  CloudIcon,
   CogIcon,
   FilmIcon,
   PlayIcon,
+  TicketIcon,
 } from '@heroicons/react/outline';
 import {
   CheckCircleIcon,
@@ -12,6 +14,7 @@ import {
   ExternalLinkIcon,
 } from '@heroicons/react/solid';
 import axios from 'axios';
+import { uniqBy } from 'lodash';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useMemo, useState } from 'react';
@@ -49,7 +52,8 @@ import StatusBadge from '../StatusBadge';
 
 const messages = defineMessages({
   originaltitle: 'Original Title',
-  releasedate: 'Release Date',
+  releasedate:
+    '{releaseCount, plural, one {Release Date} other {Release Dates}}',
   revenue: 'Revenue',
   budget: 'Budget',
   watchtrailer: 'Watch Trailer',
@@ -179,20 +183,28 @@ const MovieDetails: React.FC<MovieDetailsProps> = ({ movie }) => {
     : settings.currentSettings.region
     ? settings.currentSettings.region
     : 'US';
+
+  const releases = data.releases.results.find((r) => r.iso_3166_1 === region)
+    ?.release_dates;
+
+  // Release date types:
+  // 1. Premiere
+  // 2. Theatrical (limited)
+  // 3. Theatrical
+  // 4. Digital
+  // 5. Physical
+  // 6. TV
+  const filteredReleases = uniqBy(
+    releases?.filter((r) => r.type > 2 && r.type < 6),
+    'type'
+  );
+
   const movieAttributes: React.ReactNode[] = [];
 
-  if (
-    data.releases.results.length &&
-    (data.releases.results.find((r) => r.iso_3166_1 === region)
-      ?.release_dates[0].certification ||
-      data.releases.results[0].release_dates[0].certification)
-  ) {
+  const certification = releases?.find((r) => r.certification)?.certification;
+  if (certification) {
     movieAttributes.push(
-      <span className="p-0.5 py-0 border rounded-md">
-        {data.releases.results.find((r) => r.iso_3166_1 === region)
-          ?.release_dates[0].certification ||
-          data.releases.results[0].release_dates[0].certification}
-      </span>
+      <span className="p-0.5 py-0 border rounded-md">{certification}</span>
     );
   }
 
@@ -577,17 +589,57 @@ const MovieDetails: React.FC<MovieDetailsProps> = ({ movie }) => {
               <span>{intl.formatMessage(globalMessages.status)}</span>
               <span className="media-fact-value">{data.status}</span>
             </div>
-            {data.releaseDate && (
+            {filteredReleases && filteredReleases.length > 0 ? (
               <div className="media-fact">
-                <span>{intl.formatMessage(messages.releasedate)}</span>
-                <span className="media-fact-value">
-                  {intl.formatDate(data.releaseDate, {
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric',
+                <span>
+                  {intl.formatMessage(messages.releasedate, {
+                    releaseCount: filteredReleases.length,
                   })}
                 </span>
+                <span className="media-fact-value">
+                  {filteredReleases.map((r, i) => (
+                    <span
+                      className="flex items-center justify-end"
+                      key={`release-date-${i}`}
+                    >
+                      {r.type === 3 ? (
+                        // Theatrical
+                        <TicketIcon className="w-4 h-4" />
+                      ) : r.type === 4 ? (
+                        // Digital
+                        <CloudIcon className="w-4 h-4" />
+                      ) : (
+                        // Physical
+                        <FilmIcon className="w-4 h-4" />
+                      )}
+                      <span className="ml-1.5">
+                        {intl.formatDate(r.release_date, {
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric',
+                        })}
+                      </span>
+                    </span>
+                  ))}
+                </span>
               </div>
+            ) : (
+              data.releaseDate && (
+                <div className="media-fact">
+                  <span>
+                    {intl.formatMessage(messages.releasedate, {
+                      releaseCount: 1,
+                    })}
+                  </span>
+                  <span className="media-fact-value">
+                    {intl.formatDate(data.releaseDate, {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    })}
+                  </span>
+                </div>
+              )
             )}
             {data.revenue > 0 && (
               <div className="media-fact">
