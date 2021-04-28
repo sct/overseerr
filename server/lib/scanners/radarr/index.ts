85899ab49a27542390e91443531905737224338d
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-04-27 20:53:52 -0400

    fix(radarr): only process Radarr movies which are either monitored or downloaded (#1511)
    * fix(radarr): only process Radarr movies which are either monitored or downloaded
    
    * refactor: move monitored check to processRadarrMovie

--- server/lib/scanners/radarr/index.ts
+++ server/lib/scanners/radarr/index.ts
@@ -72,6 +72,17 @@ class RadarrScanner
   }
 
   private async processRadarrMovie(radarrMovie: RadarrMovie): Promise<void> {
+    if (!radarrMovie.monitored && !radarrMovie.downloaded) {
+      this.log(
+        'Title is unmonitored and has not been downloaded. Skipping item.',
+        'debug',
+        {
+          title: radarrMovie.title,
+        }
+      );
+      return;
+    }
+
     try {
       const server4k = this.enable4kMovie && this.currentServer.is4k;
       await this.processMovie(radarrMovie.tmdbId, {
