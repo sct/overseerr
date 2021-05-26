d062e2a0782f5bd6790432020c21f4fc2e980a58
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-05-26 00:34:44 -0400

    fix(frontend): add missing route guards to settings pages

--- src/pages/settings/notifications/email.tsx
+++ src/pages/settings/notifications/email.tsx
@@ -1,10 +1,13 @@
 import { NextPage } from 'next';
 import React from 'react';
+import NotificationsEmail from '../../../components/Settings/Notifications/NotificationsEmail';
 import SettingsLayout from '../../../components/Settings/SettingsLayout';
 import SettingsNotifications from '../../../components/Settings/SettingsNotifications';
-import NotificationsEmail from '../../../components/Settings/Notifications/NotificationsEmail';
+import useRouteGuard from '../../../hooks/useRouteGuard';
+import { Permission } from '../../../hooks/useUser';
 
 const NotificationsPage: NextPage = () => {
+  useRouteGuard(Permission.MANAGE_SETTINGS);
   return (
     <SettingsLayout>
       <SettingsNotifications>
--- src/pages/settings/notifications/pushbullet.tsx
+++ src/pages/settings/notifications/pushbullet.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 import NotificationsPushbullet from '../../../components/Settings/Notifications/NotificationsPushbullet';
 import SettingsLayout from '../../../components/Settings/SettingsLayout';
 import SettingsNotifications from '../../../components/Settings/SettingsNotifications';
+import useRouteGuard from '../../../hooks/useRouteGuard';
+import { Permission } from '../../../hooks/useUser';
 
 const NotificationsPage: NextPage = () => {
+  useRouteGuard(Permission.MANAGE_SETTINGS);
   return (
     <SettingsLayout>
       <SettingsNotifications>
