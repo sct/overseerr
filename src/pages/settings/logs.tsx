78fc1f7b7d9ef912077066a3605fed6237fb4c8a
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-05-30 20:18:38 -0400

    fix(frontend): add missing route guards to settings pages (#1700)

--- src/pages/settings/logs.tsx
+++ src/pages/settings/logs.tsx
@@ -2,8 +2,11 @@ import { NextPage } from 'next';
 import React from 'react';
 import SettingsLayout from '../../components/Settings/SettingsLayout';
 import SettingsLogs from '../../components/Settings/SettingsLogs';
+import useRouteGuard from '../../hooks/useRouteGuard';
+import { Permission } from '../../hooks/useUser';
 
 const SettingsLogsPage: NextPage = () => {
+  useRouteGuard(Permission.MANAGE_SETTINGS);
   return (
     <SettingsLayout>
       <SettingsLogs />
