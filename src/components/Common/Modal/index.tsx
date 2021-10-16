084a842a4f9b6caaed22edbe77bc9e414bc1f387
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-10-16 11:46:18 -0400

    fix(ui): refinements for 'About' page (#2173)
    * fix(ui): refinements for 'About' page
    
    * fix: remove unneeded GithubLink function
    
    * fix: display/link badges appropriately

--- src/components/Common/Modal/index.tsx
+++ src/components/Common/Modal/index.tsx
@@ -128,16 +128,16 @@ const Modal: React.FC<ModalProps> = ({
               />
             </div>
           )}
-          <div className="relative sm:flex sm:items-center">
+          <div className="relative overflow-x-hidden sm:flex sm:items-center">
             {iconSvg && <div className="modal-icon">{iconSvg}</div>}
             <div
-              className={`mt-3 text-center sm:mt-0 sm:text-left ${
+              className={`mt-3 text-center sm:mt-0 sm:text-left truncate text-white ${
                 iconSvg ? 'sm:ml-4' : 'sm:mb-4'
               }`}
             >
               {title && (
                 <span
-                  className="text-lg font-bold leading-6 text-white"
+                  className="text-lg font-bold leading-6 truncate"
                   id="modal-headline"
                 >
                   {title}
