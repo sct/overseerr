83c54a394022c4c796e8506faaa72d6a18635cfa
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-01-27 07:30:10 -0500

    refactor: appease Prettier

--- src/components/Common/Modal/index.tsx
+++ src/components/Common/Modal/index.tsx
@@ -130,7 +130,7 @@ const Modal: React.FC<ModalProps> = ({
               />
             </div>
           )}
-          <div className="relative p-0.5 overflow-x-hidden sm:flex sm:items-center">
+          <div className="relative overflow-x-hidden p-0.5 sm:flex sm:items-center">
             {iconSvg && <div className="modal-icon">{iconSvg}</div>}
             <div
               className={`mt-3 truncate text-center text-white sm:mt-0 sm:text-left ${
