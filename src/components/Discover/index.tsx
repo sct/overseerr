d435418d3b39a411250100deba84ca8dd7afd41e
Ryan Cohen <ryan@sct.dev>
2022-08-03 04:04:43 +0000

    fix: formatting

--- src/components/Discover/index.tsx
+++ src/components/Discover/index.tsx
@@ -2,7 +2,7 @@ import { ArrowCircleRightIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
 import { defineMessages, useIntl } from 'react-intl';
 import useSWR from 'swr';
-import { WatchlistItem } from '../../../server/interfaces/api/discoverInterfaces';
+import type { WatchlistItem } from '../../../server/interfaces/api/discoverInterfaces';
 import type { MediaResultsResponse } from '../../../server/interfaces/api/mediaInterfaces';
 import type { RequestResultsResponse } from '../../../server/interfaces/api/requestInterfaces';
 import { Permission, useUser } from '../../hooks/useUser';
