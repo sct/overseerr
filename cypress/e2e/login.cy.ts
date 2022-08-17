d1f9366deca4c403894520fe4768db921db90d79
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-08-16 22:54:23 -0700

    test: update login test to check for Trending instead of Recently Added

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -2,12 +2,12 @@ describe('Login Page', () => {
   it('succesfully logs in as an admin', () => {
     cy.login(Cypress.env('ADMIN_EMAIL'), Cypress.env('ADMIN_PASSWORD'));
     cy.visit('/');
-    cy.contains('Recently Added');
+    cy.contains('Trending');
   });
 
   it('succesfully logs in as a local user', () => {
     cy.login(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'));
     cy.visit('/');
-    cy.contains('Recently Added');
+    cy.contains('Trending');
   });
 });
