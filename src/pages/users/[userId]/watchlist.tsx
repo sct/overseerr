ebec1590a063f8d043c047cb34a5281075613d34
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-08-21 20:19:22 -0700

    refactor: yarn format

--- src/pages/users/[userId]/watchlist.tsx
+++ src/pages/users/[userId]/watchlist.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next';
 import DiscoverWatchlist from '@app/components/Discover/DiscoverWatchlist';
 import useRouteGuard from '@app/hooks/useRouteGuard';
 import { Permission } from '@app/hooks/useUser';
+import type { NextPage } from 'next';
 
 const UserRequestsPage: NextPage = () => {
   useRouteGuard(Permission.WATCHLIST_VIEW);
