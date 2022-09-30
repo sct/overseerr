ad88048a94cde9bc2b92ebc02b833284d8b28aea
Ryan Cohen <ryan@sct.dev>
2022-09-30 15:55:48 +0900

    fix: make sure cachedimage component only works with tmdb urls

--- src/components/Common/CachedImage/index.tsx
+++ src/components/Common/CachedImage/index.tsx
@@ -7,21 +7,18 @@ const imageLoader: ImageLoader = ({ src }) => src;
 /**
  * The CachedImage component should be used wherever
  * we want to offer the option to locally cache images.
- *
- * It uses the `next/image` Image component but overrides
- * the `unoptimized` prop based on the application setting `cacheImages`.
  **/
 const CachedImage = ({ src, ...props }: ImageProps) => {
   const { currentSettings } = useSettings();
 
   let imageUrl = src;
 
-  if (
-    typeof imageUrl === 'string' &&
-    imageUrl.startsWith('https://image.tmdb.org') &&
-    currentSettings.cacheImages
-  ) {
-    imageUrl = imageUrl.replace('https://image.tmdb.org', '/imageproxy');
+  if (typeof imageUrl === 'string') {
+    const parsedUrl = new URL(imageUrl);
+
+    if (parsedUrl.host === 'image.tmdb.org' && currentSettings.cacheImages) {
+      imageUrl = imageUrl.replace('https://image.tmdb.org', '/imageproxy');
+    }
   }
 
   return <Image unoptimized loader={imageLoader} src={imageUrl} {...props} />;
