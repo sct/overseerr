f3730b00cea1a51d7cdad3c8e3219e09afb943ff
sct <ryan@sct.dev>
2023-01-11 16:03:22 +0900

    feat: update discover sliders to use new browse page

--- src/components/Discover/TvGenreSlider/index.tsx
+++ src/components/Discover/TvGenreSlider/index.tsx
@@ -43,7 +43,7 @@ const TvGenreSlider = () => {
             image={`https://image.tmdb.org/t/p/w1280_filter(duotone,${
               genreColorMap[genre.id] ?? genreColorMap[0]
             })${genre.backdrops[4]}`}
-            url={`/discover/tv/genre/${genre.id}`}
+            url={`/discover/tv?genre=${genre.id}`}
           />
         ))}
         placeholder={<GenreCard.Placeholder />}
