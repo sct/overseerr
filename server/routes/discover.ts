38e8c5eccb45388f30bf6857322de2757db061c9
Danshil Kokil Mungur <me@danshilm.com>
2023-02-24 04:10:33 +0400

    fix(discover): correctly load additional watchlist items pages

--- server/routes/discover.ts
+++ server/routes/discover.ts
@@ -805,7 +805,7 @@ discoverRoutes.get<{ page?: number }, WatchlistResponse>(
   async (req, res) => {
     const userRepository = getRepository(User);
     const itemsPerPage = 20;
-    const page = req.params.page ?? 1;
+    const page = Number(req.query.page) ?? 1;
     const offset = (page - 1) * itemsPerPage;
 
     const activeUser = await userRepository.findOne({
@@ -829,8 +829,8 @@ discoverRoutes.get<{ page?: number }, WatchlistResponse>(
 
     return res.json({
       page,
-      totalPages: Math.ceil(watchlist.size / itemsPerPage),
-      totalResults: watchlist.size,
+      totalPages: Math.ceil(watchlist.totalSize / itemsPerPage),
+      totalResults: watchlist.totalSize,
       results: watchlist.items.map((item) => ({
         ratingKey: item.ratingKey,
         title: item.title,
