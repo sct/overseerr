6ea880611c1219b97f161a1fdd8e252f42d92828
Brandon <cohbrandon@gmail.com>
2023-03-27 18:13:45 -0400

    fix: added better scroll lock functionality

--- src/components/Layout/PullToRefresh/index.tsx
+++ src/components/Layout/PullToRefresh/index.tsx
@@ -1,4 +1,3 @@
-import { useLockBodyScroll } from '@app/hooks/useLockBodyScroll';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
@@ -8,7 +7,6 @@ const PullToRefresh = () => {
 
   const [pullStartPoint, setPullStartPoint] = useState(0);
   const [pullChange, setPullChange] = useState(0);
-  const [unlockScreen, setUnlockScreen] = useState(true);
   const refreshDiv = useRef<HTMLDivElement>(null);
 
   const pullDownInitThreshold = pullChange > 20;
@@ -26,20 +24,24 @@ const PullToRefresh = () => {
 
     const html = document.querySelector('html');
 
-    if (html) {
-      html.style.overscrollBehaviorY = 'none';
-    }
-
     const pullStart = (e: TouchEvent) => {
-      e.preventDefault();
       setPullStartPoint(e.targetTouches[0].screenY);
+
+      if (window.scrollY === 0 && window.scrollX === 0) {
+        document.body.style.touchAction = 'none';
+        document.body.style.overscrollBehavior = 'none';
+        if (html) {
+          html.style.overscrollBehaviorY = 'none';
+        }
+      }
     };
 
-    const pullDown = (e: TouchEvent) => {
+    const pullDown = async (e: TouchEvent) => {
       const screenY = e.targetTouches[0].screenY;
 
       const pullLength =
         pullStartPoint < screenY ? Math.abs(screenY - pullStartPoint) : 0;
+
       setPullChange(pullLength);
     };
 
@@ -51,56 +53,24 @@ const PullToRefresh = () => {
       } else {
         setPullChange(0);
       }
+
+      document.body.style.touchAction = 'auto';
+      document.body.style.overscrollBehaviorY = 'auto';
+      if (html) {
+        html.style.overscrollBehaviorY = 'auto';
+      }
     };
 
-    window.addEventListener('touchstart', pullStart);
-    window.addEventListener('touchmove', pullDown);
-    window.addEventListener('touchend', pullFinish);
+    window.addEventListener('touchstart', pullStart, { passive: false });
+    window.addEventListener('touchmove', pullDown, { passive: false });
+    window.addEventListener('touchend', pullFinish, { passive: false });
 
     return () => {
       window.removeEventListener('touchstart', pullStart);
       window.removeEventListener('touchmove', pullDown);
       window.removeEventListener('touchend', pullFinish);
     };
-  }, [
-    pullChange,
-    pullDownReloadThreshold,
-    pullStartPoint,
-    router,
-    unlockScreen,
-  ]);
-
-  const isIconVisible = async (element: HTMLDivElement) => {
-    return new Promise((resolve) => {
-      const observer = new IntersectionObserver(([entry]) => {
-        resolve(entry.intersectionRatio === 1);
-        observer.disconnect();
-      });
-      observer.observe(element);
-    });
-  };
-
-  useEffect(() => {
-    const refreshIcon = document.getElementById(
-      'refreshIcon'
-    ) as HTMLDivElement;
-
-    const checkIfVisible = async () => {
-      if ((await isIconVisible(refreshIcon)) && pullDownInitThreshold) {
-        setUnlockScreen(false);
-      } else {
-        setUnlockScreen(true);
-      }
-    };
-
-    window.addEventListener('touchmove', checkIfVisible);
-
-    return () => {
-      window.removeEventListener('touchmove', checkIfVisible);
-    };
-  }, [pullChange, pullDownInitThreshold]);
-
-  useLockBodyScroll(true, unlockScreen);
+  }, [pullDownInitThreshold, pullDownReloadThreshold, pullStartPoint, router]);
 
   return (
     <div
@@ -120,6 +90,7 @@ const PullToRefresh = () => {
         className={`${
           refreshDiv.current?.classList[9] === 'loading' && 'animate-spin'
         } relative -top-24 h-9 w-9 rounded-full border-4 border-gray-800 bg-gray-800 shadow-md shadow-black ring-1 ring-gray-700`}
+        style={{ animationDirection: 'reverse' }}
       >
         <ArrowPathIcon
           className={`rounded-full ${
