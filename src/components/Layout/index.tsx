b56f925567640a0a7d2fabed6b498c7d233c5302
Brandon <cohbrandon@gmail.com>
2023-05-10 21:51:35 -0400

    fix: changed to allow usage on all touch devices

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -61,9 +61,7 @@ const Layout = ({ children }: LayoutProps) => {
       </div>
 
       <div className="relative mb-16 flex w-0 min-w-0 flex-1 flex-col lg:ml-64">
-        <div className="sm:hidden">
-          <PullToRefresh />
-        </div>
+        <PullToRefresh />
         <div
           className={`searchbar fixed left-0 right-0 top-0 z-10 flex flex-shrink-0 bg-opacity-80 transition duration-300 ${
             isScrolled ? 'bg-gray-700' : 'bg-transparent'
