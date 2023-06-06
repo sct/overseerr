c33b4ffd3b00ba3050d593a2f4e41ee312d70e8b
Andrew Kennedy <andrew-kennedy@users.noreply.github.com>
2023-06-05 19:50:02 -0700

    Make a (shallow) copy of radarr/sonarr tags into a request before adding user tags

--- server/entity/MediaRequest.ts
+++ server/entity/MediaRequest.ts
@@ -36,10 +36,10 @@ import Media from './Media';
 import SeasonRequest from './SeasonRequest';
 import { User } from './User';
 
-export class RequestPermissionError extends Error {}
-export class QuotaRestrictedError extends Error {}
-export class DuplicateMediaRequestError extends Error {}
-export class NoSeasonsAvailableError extends Error {}
+export class RequestPermissionError extends Error { }
+export class QuotaRestrictedError extends Error { }
+export class DuplicateMediaRequestError extends Error { }
+export class NoSeasonsAvailableError extends Error { }
 
 type MediaRequestOptions = {
   isAutoRequest?: boolean;
@@ -91,8 +91,7 @@ export class MediaRequest {
       )
     ) {
       throw new RequestPermissionError(
-        `You do not have permission to make ${
-          requestBody.is4k ? '4K ' : ''
+        `You do not have permission to make ${requestBody.is4k ? '4K ' : ''
         }movie requests.`
       );
     } else if (
@@ -107,8 +106,7 @@ export class MediaRequest {
       )
     ) {
       throw new RequestPermissionError(
-        `You do not have permission to make ${
-          requestBody.is4k ? '4K ' : ''
+        `You do not have permission to make ${requestBody.is4k ? '4K ' : ''
         }series requests.`
       );
     }
@@ -247,8 +245,8 @@ export class MediaRequest {
       const requestedSeasons =
         requestBody.seasons === 'all'
           ? tmdbMediaShow.seasons
-              .map((season) => season.season_number)
-              .filter((sn) => sn > 0)
+            .map((season) => season.season_number)
+            .filter((sn) => sn > 0)
           : (requestBody.seasons as number[]);
       let existingSeasons: number[] = [];
 
@@ -575,7 +573,7 @@ export class MediaRequest {
       // Do not update the status if the item is already partially available or available
       media[this.is4k ? 'status4k' : 'status'] !== MediaStatus.AVAILABLE &&
       media[this.is4k ? 'status4k' : 'status'] !==
-        MediaStatus.PARTIALLY_AVAILABLE
+      MediaStatus.PARTIALLY_AVAILABLE
     ) {
       media[this.is4k ? 'status4k' : 'status'] = MediaStatus.PROCESSING;
       mediaRepository.save(media);
@@ -688,10 +686,8 @@ export class MediaRequest {
 
         if (!radarrSettings) {
           logger.warn(
-            `There is no default ${
-              this.is4k ? '4K ' : ''
-            }Radarr server configured. Did you set any of your ${
-              this.is4k ? '4K ' : ''
+            `There is no default ${this.is4k ? '4K ' : ''
+            }Radarr server configured. Did you set any of your ${this.is4k ? '4K ' : ''
             }Radarr servers as default?`,
             {
               label: 'Media Request',
@@ -704,7 +700,7 @@ export class MediaRequest {
 
         let rootFolder = radarrSettings.activeDirectory;
         let qualityProfile = radarrSettings.activeProfileId;
-        let tags = radarrSettings.tags;
+        let tags = [...radarrSettings.tags];
 
         if (
           this.rootFolder &&
@@ -923,10 +919,8 @@ export class MediaRequest {
 
         if (!sonarrSettings) {
           logger.warn(
-            `There is no default ${
-              this.is4k ? '4K ' : ''
-            }Sonarr server configured. Did you set any of your ${
-              this.is4k ? '4K ' : ''
+            `There is no default ${this.is4k ? '4K ' : ''
+            }Sonarr server configured. Did you set any of your ${this.is4k ? '4K ' : ''
             }Sonarr servers as default?`,
             {
               label: 'Media Request',
@@ -1001,8 +995,8 @@ export class MediaRequest {
             : sonarrSettings.activeLanguageProfileId;
         let tags =
           seriesType === 'anime'
-            ? sonarrSettings.animeTags
-            : sonarrSettings.tags;
+            ? [...sonarrSettings.animeTags]
+            : [...sonarrSettings.tags];
 
         if (
           this.rootFolder &&
@@ -1178,16 +1172,14 @@ export class MediaRequest {
           event = `New ${this.is4k ? '4K ' : ''}${mediaType} Request`;
           break;
         case Notification.MEDIA_AUTO_REQUESTED:
-          event = `${
-            this.is4k ? '4K ' : ''
-          }${mediaType} Request Automatically Submitted`;
+          event = `${this.is4k ? '4K ' : ''
+            }${mediaType} Request Automatically Submitted`;
           notifyAdmin = false;
           notifySystem = false;
           break;
         case Notification.MEDIA_AUTO_APPROVED:
-          event = `${
-            this.is4k ? '4K ' : ''
-          }${mediaType} Request Automatically Approved`;
+          event = `${this.is4k ? '4K ' : ''
+            }${mediaType} Request Automatically Approved`;
           break;
         case Notification.MEDIA_FAILED:
           event = `${this.is4k ? '4K ' : ''}${mediaType} Request Failed`;
@@ -1203,9 +1195,8 @@ export class MediaRequest {
           notifySystem,
           notifyUser: notifyAdmin ? undefined : this.requestedBy,
           event,
-          subject: `${movie.title}${
-            movie.release_date ? ` (${movie.release_date.slice(0, 4)})` : ''
-          }`,
+          subject: `${movie.title}${movie.release_date ? ` (${movie.release_date.slice(0, 4)})` : ''
+            }`,
           message: truncate(movie.overview, {
             length: 500,
             separator: /\s/,
@@ -1222,9 +1213,8 @@ export class MediaRequest {
           notifySystem,
           notifyUser: notifyAdmin ? undefined : this.requestedBy,
           event,
-          subject: `${tv.name}${
-            tv.first_air_date ? ` (${tv.first_air_date.slice(0, 4)})` : ''
-          }`,
+          subject: `${tv.name}${tv.first_air_date ? ` (${tv.first_air_date.slice(0, 4)})` : ''
+            }`,
           message: truncate(tv.overview, {
             length: 500,
             separator: /\s/,
