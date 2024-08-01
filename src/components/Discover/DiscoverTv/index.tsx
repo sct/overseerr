12ca6aa9bcdbc59c5eb5e71a3315c93549bdcdef
Leejay Hsu <37034741+leejayhsu@users.noreply.github.com>
2024-08-01 00:35:42 -0700

    feat: persistent filters

--- src/components/Discover/DiscoverTv/index.tsx
+++ src/components/Discover/DiscoverTv/index.tsx
@@ -9,6 +9,7 @@ import {
 } from '@app/components/Discover/constants';
 import FilterSlideover from '@app/components/Discover/FilterSlideover';
 import useDiscover from '@app/hooks/useDiscover';
+import usePersistentFilters from '@app/hooks/usePersistentFilteres';
 import { useUpdateQueryParams } from '@app/hooks/useUpdateQueryParams';
 import Error from '@app/pages/_error';
 import { BarsArrowDownIcon, FunnelIcon } from '@heroicons/react/24/solid';
@@ -49,6 +50,7 @@ const DiscoverTv = () => {
   const [showFilters, setShowFilters] = useState(false);
   const preparedFilters = prepareFilterValues(router.query);
   const updateQueryParams = useUpdateQueryParams({});
+  usePersistentFilters('dtv-filter-settings');
 
   const {
     isLoadingInitialData,
