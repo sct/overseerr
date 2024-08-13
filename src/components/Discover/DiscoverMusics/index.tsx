bb1a14e7147ea7e6810c46c10eb3811496894846
Anatole Sot <anatolesot@gmail.com>
2024-08-13 19:36:12 +0200

    feat(discovermusics): modified the front-end of the Musics Page
    As there is no music discovery service I found which would work well with the current system I
    decided to modify the current Music Discovery Page to show off your downloaded Artists instead of
    nothing

--- src/components/Discover/DiscoverMusics/index.tsx
+++ src/components/Discover/DiscoverMusics/index.tsx
@@ -1,94 +1,94 @@
-import Button from '@app/components/Common/Button';
 import Header from '@app/components/Common/Header';
 import ListView from '@app/components/Common/ListView';
 import PageTitle from '@app/components/Common/PageTitle';
-import type { FilterOptions } from '@app/components/Discover/constants';
-import {
-  countActiveFilters,
-  prepareFilterValues,
-} from '@app/components/Discover/constants';
-import FilterSlideover from '@app/components/Discover/FilterSlideover';
-import RecentlyAddedSlider from '@app/components/Discover/RecentlyAddedSlider';
-import useDiscover from '@app/hooks/useDiscover';
 import Error from '@app/pages/_error';
-import { FunnelIcon } from '@heroicons/react/24/solid';
-import type { ArtistResult, ReleaseResult } from '@server/models/Search';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { mbArtistType } from '@server/api/musicbrainz/interfaces';
+import type Media from '@server/entity/Media';
+import type { MediaResultsResponse } from '@server/interfaces/api/mediaInterfaces';
+import type { ArtistResult } from '@server/models/Search';
+import { useMemo, useState } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
+import useSWR from 'swr';
 
 const messages = defineMessages({
-  discovermusics: 'Musics',
-  activefilters:
-    '{count, plural, one {# Active Filter} other {# Active Filters}}',
-  discovermoremusics: 'Discover More Musics',
+  discovermusics: {
+    defaultMessage: 'Your Downloaded Artists',
+    id: 'discover.downloadedmusics',
+  },
 });
+
+const mediaResultsToArtistResults = (results: Media[]): ArtistResult[] => {
+  return results.map((media) => ({
+    id: media.mbId ?? '',
+    name: media.title ?? '...',
+    type: mbArtistType.OTHER,
+    mediaType: 'artist',
+    mediaInfo: media,
+    releases: [],
+    tags: [],
+  }));
+};
 
 const DiscoverMusics = () => {
   const intl = useIntl();
-  const router = useRouter();
 
-  const preparedFilters = prepareFilterValues(router.query);
+  const [artists, setArtists] = useState<ArtistResult[]>([]);
+  const [isLoadingInitialData, setIsLoadingInitialData] = useState(true);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const [isReachingEnd, setIsReachingEnd] = useState(false);
 
-  const {
-    isLoadingInitialData,
-    isEmpty,
-    isLoadingMore,
-    isReachingEnd,
-    titles,
-    fetchMore,
-    error,
-  } = useDiscover<ReleaseResult | ArtistResult, unknown, FilterOptions>(
-    '/api/v1/discover/musics',
-    preparedFilters
-  );
-  const [showFilters, setShowFilters] = useState(false);
-
-  if (error || !titles) {
-    return <Error statusCode={500} />;
-  }
+  const isEmpty = useMemo(() => artists.length === 0, [artists]);
 
   const title = intl.formatMessage(messages.discovermusics);
+
+  const fetchMore = () => {
+    setIsLoadingMore(true);
+    fetch(
+      '/api/v1/media?filter=available&type=artist&sort=mediaAdded&skip=' +
+        artists.length
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const results = mediaResultsToArtistResults(data.results);
+        if (results.length === 0) {
+          setIsReachingEnd(true);
+        } else {
+          setArtists((prev) => [...prev, ...results]);
+        }
+      })
+      .finally(() => {
+        setIsLoadingMore(false);
+      });
+  };
+
+  const { error } = useSWR<MediaResultsResponse>(
+    '/api/v1/media?filter=available&type=artist&sort=mediaAdded',
+    {
+      onSuccess: (data) => {
+        const results = mediaResultsToArtistResults(data.results);
+        setArtists(results);
+        setIsLoadingInitialData(false);
+      },
+    }
+  );
+
+  if (error) {
+    return <Error statusCode={500} />;
+  }
 
   return (
     <>
       <PageTitle title={title} />
       <div className="mb-4 flex flex-col justify-between lg:flex-row lg:items-end">
         <Header>{title}</Header>
-        <div className="mt-2 flex flex-grow flex-col sm:flex-row lg:flex-grow-0">
-          <FilterSlideover
-            type="music"
-            currentFilters={preparedFilters}
-            onClose={() => setShowFilters(false)}
-            show={showFilters}
-          />
-          <div className="mb-2 flex flex-grow sm:mb-0 lg:flex-grow-0">
-            <Button onClick={() => setShowFilters(true)} className="w-full">
-              <FunnelIcon />
-              <span>
-                {intl.formatMessage(messages.activefilters, {
-                  count: countActiveFilters(preparedFilters),
-                })}
-              </span>
-            </Button>
-          </div>
-        </div>
-      </div>
-      {Object.keys(preparedFilters).length === 0 && (
-        <RecentlyAddedSlider type="artist" />
-      )}
-      <div className="slider-header">
-        <div className="slider-title">
-          <span>{intl.formatMessage(messages.discovermoremusics)}</span>
-        </div>
       </div>
       <ListView
-        items={titles}
+        items={artists}
         isEmpty={isEmpty}
-        isLoading={
-          isLoadingInitialData || (isLoadingMore && (titles?.length ?? 0) > 0)
-        }
         isReachingEnd={isReachingEnd}
+        isLoading={
+          isLoadingInitialData || (isLoadingMore && (artists?.length ?? 0) > 0)
+        }
         onScrollBottom={fetchMore}
       />
     </>
