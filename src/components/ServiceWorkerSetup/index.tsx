f0af3ca61d19b25767e21e8c94b1bcbc679d1060
Brandon Cohen <brandon@z3hn.dev>
2025-02-27 22:14:48 -0600

    Revert "fix: improved web push management (#3421)" (#4058)

--- src/components/ServiceWorkerSetup/index.tsx
+++ src/components/ServiceWorkerSetup/index.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-console */
-
+import useSettings from '@app/hooks/useSettings';
 import { useUser } from '@app/hooks/useUser';
+import axios from 'axios';
 import { useEffect } from 'react';
 
 const ServiceWorkerSetup = () => {
+  const { currentSettings } = useSettings();
   const { user } = useUser();
   useEffect(() => {
     if ('serviceWorker' in navigator && user?.id) {
@@ -14,12 +16,33 @@ const ServiceWorkerSetup = () => {
             '[SW] Registration successful, scope is:',
             registration.scope
           );
+
+          if (currentSettings.enablePushRegistration) {
+            const sub = await registration.pushManager.subscribe({
+              userVisibleOnly: true,
+              applicationServerKey: currentSettings.vapidPublic,
+            });
+
+            const parsedSub = JSON.parse(JSON.stringify(sub));
+
+            if (parsedSub.keys.p256dh && parsedSub.keys.auth) {
+              await axios.post('/api/v1/user/registerPushSubscription', {
+                endpoint: parsedSub.endpoint,
+                p256dh: parsedSub.keys.p256dh,
+                auth: parsedSub.keys.auth,
+              });
+            }
+          }
         })
         .catch(function (error) {
           console.log('[SW] Service worker registration failed, error:', error);
         });
     }
-  }, [user]);
+  }, [
+    user,
+    currentSettings.vapidPublic,
+    currentSettings.enablePushRegistration,
+  ]);
   return null;
 };
 
