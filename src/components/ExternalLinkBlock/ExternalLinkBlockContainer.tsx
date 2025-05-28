748d7c915f665dd200877089585b646120aa4406
kage1414 <kylejohnson92294@gmail.com>
2025-05-28 17:11:56 -0400

    refactor(external link block): cleanup naming

--- src/components/ExternalLinkBlock/ExternalLinkBlockContainer.tsx
+++ src/components/ExternalLinkBlock/ExternalLinkBlockContainer.tsx
@@ -1,7 +1,7 @@
 import type { ExternalLinkBlockProps } from '@app/components/ExternalLinkBlock/ExternalLinkBlockItems';
 import { ExternalLinkBlockItems } from '@app/components/ExternalLinkBlock/ExternalLinkBlockItems';
 
-export const ExternalLinkBlock = (props: ExternalLinkBlockProps) => {
+export const ExternalLinkBlockContainer = (props: ExternalLinkBlockProps) => {
   return (
     <div className="flex w-full items-center justify-center space-x-5">
       <ExternalLinkBlockItems {...props} />
--- src/components/ExternalLinkBlock/index.tsx
+++ src/components/ExternalLinkBlock/index.tsx
@@ -1,3 +1,3 @@
-import { ExternalLinkBlock } from '@app/components/ExternalLinkBlock/ExternalLinkBlockContainer';
+import { ExternalLinkBlockContainer as ExternalLinkBlock } from '@app/components/ExternalLinkBlock/ExternalLinkBlockContainer';
 
 export default ExternalLinkBlock;
