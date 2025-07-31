44651e5f5dcdd18de40f0a9ecef42efec486e984
Denys Dovhan <denysdovhan@gmail.com>
2025-07-31 17:32:56 +0300

    Fix mobile-web-app-capable warning

--- src/components/PWAHeader/index.tsx
+++ src/components/PWAHeader/index.tsx
@@ -152,7 +152,7 @@ const PWAHeader = ({ applicationTitle = 'Overseerr' }: PWAHeaderProps) => {
         href="/apple-splash-1136-640.jpg"
         media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
       />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
       <meta
         name="apple-mobile-web-app-status-bar-style"
         content="black-translucent"
