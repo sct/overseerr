f049588c79e603dec91920db0895683e831e818e
adsbikes95 <ads0595@gmail.com>
2026-01-28 16:04:51 -0700

    chore(ui): adjust search input padding

--- src/components/Layout/SearchInput/index.tsx
+++ src/components/Layout/SearchInput/index.tsx
@@ -22,8 +22,9 @@ const SearchInput = () => {
           </div>
           <input
             id="search_field"
-            style={{ paddingRight: searchValue.length > 0 ? '1.75rem' : '' }}
-            className="block w-full rounded-full border border-gray-600 bg-gray-900 bg-opacity-80 py-2 pl-10 text-white placeholder-gray-300 hover:border-gray-500 focus:border-gray-500 focus:bg-opacity-100 focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-base"
+            className={`block w-full rounded-full border border-gray-600 bg-gray-900 bg-opacity-80 py-2 pl-10 text-white placeholder-gray-300 hover:border-gray-500 focus:border-gray-500 focus:bg-opacity-100 focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-base ${
+              searchValue.length > 0 ? 'pr-7' : ''
+            }`}
             placeholder={intl.formatMessage(messages.searchPlaceholder)}
             type="search"
             autoComplete="off"
